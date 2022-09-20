6232e0bec33bacc5ec8d953404823f7da5f20aac
Christian John Felix <christianfelix@getzclinical.com>
2022-09-20 10:30:58 +0800

    Updated

--- Transports/DepartmentGroupTransport.cs
+++ Transports/DepartmentGroupTransport.cs
@@ -5,6 +5,7 @@ namespace DemoApplication.WebAPI.Transports
 {
     public class DepartmentGroupTransport : BaseCodeTransport, IRegister
     {
+        public long DepartmentId { get; set; }
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<DepartmentGroup, DepartmentGroupTransport>()
