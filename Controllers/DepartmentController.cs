6825d4738c5cb3b6ad765237f9e1a3f8f32bb39b
Christian John Felix <christianfelix@getzclinical.com>
2022-09-20 10:23:42 +0800

    Added API

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DemoApplication.WebAPI.Services;
+using DemoApplication.WebAPI.Transports;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApplication.WebAPI.Controllers
@@ -7,9 +9,53 @@ namespace DemoApplication.WebAPI.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
-        public DepartmentController()
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
         {
-
+            _departmentService = departmentService;
         }
+
+        [HttpPost(template: nameof(AddDepartment), Name = nameof(AddDepartment))]
+        public async Task<IActionResult> AddDepartment([FromBody] DepartmentTransport transport)
+        {
+            if (transport == null)
+                return BadRequest("Employee is null.");
+
+            bool result = await _departmentService.Add(transport);
+
+            return Ok(result);
+        }
+
+        [HttpPatch(template: nameof(UpdateDepartment), Name = nameof(UpdateDepartment))]
+        public async Task<IActionResult> UpdateDepartment([FromBody] DepartmentTransport transport)
+        {
+            if (transport == null)
+                return BadRequest("Employee is null.");
+
+
+            bool result = await _departmentService.Update(transport);
+
+            return Ok(result);
+        }
+
+        [HttpGet(template: nameof(GetAllDepartment), Name = nameof(GetAllDepartment))]
+        public async Task<IActionResult> GetAllDepartment()
+        {
+            IEnumerable<DepartmentTransport> employees = await _departmentService.GetAll();
+
+            return Ok(employees);
+        }
+
+        [HttpGet(template: "GetDepartmentById/{id}", Name = nameof(GetDepartmentById))]
+        public async Task<IActionResult> GetDepartmentById(long id)
+        {
+            DepartmentTransport employee = await _departmentService.GetById(id);
+
+            if (employee == null)
+                return NotFound("The Employee record couldn't be found.");
+
+            return Ok(employee);
+        }
+
     }
 }
--- Controllers/DepartmentGroupController.cs
+++ Controllers/DepartmentGroupController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DemoApplication.WebAPI.Services;
+using DemoApplication.WebAPI.Transports;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApplication.WebAPI.Controllers
@@ -7,9 +9,21 @@ namespace DemoApplication.WebAPI.Controllers
     [ApiController]
     public class DepartmentGroupController : ControllerBase
     {
-        public DepartmentGroupController()
+        private readonly IDepartmentGroupService _departmentGroupService;
+        public DepartmentGroupController(IDepartmentGroupService departmentGroupService)
         {
+            _departmentGroupService = departmentGroupService;
+        }
 
+        [HttpGet(template: "GetByDepartmentId/{id}", Name = nameof(GetByDepartmentId))]
+        public async Task<IActionResult> GetByDepartmentId(long id)
+        {
+            IEnumerable<DepartmentGroupTransport> departmentGroup = await _departmentGroupService.GetByDepartmentId(id);
+
+            if (departmentGroup == null)
+                return NotFound("The Employee record couldn't be found.");
+
+            return Ok(departmentGroup);
         }
     }
 }
