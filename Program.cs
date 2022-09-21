8ee2fe7b05ed497694b4e2c0b86e1c3eb8efa438
Christian John Felix <christianfelix@getzclinical.com>
2022-09-21 18:45:10 +0800

    Updated

--- Program.cs
+++ Program.cs
@@ -82,7 +82,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
